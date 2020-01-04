811232a51093f54920b6941e2235c9d2dd727b91
nitinautomation <nitingholap.4392@gmail.com>
2020-01-05 00:23:36 +0530

    [NG]Get Request with query param and validating response code.

--- RestAPIBDDTest/src/test/java/io/restassured/test/RestAPITest/TC_001_GetGoogleMap_API_Test.java
+++ RestAPIBDDTest/src/test/java/io/restassured/test/RestAPITest/TC_001_GetGoogleMap_API_Test.java
@@ -1,10 +1,11 @@
 package io.restassured.test.RestAPITest;
 
-import java.io.IOException;
-
+import static io.restassured.RestAssured.*;
 import org.testng.annotations.Test;
-
+import static org.hamcrest.Matchers.*;
 import com.restassured.baseclass.BaseClass;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 public class TC_001_GetGoogleMap_API_Test extends BaseClass{
 
@@ -12,8 +13,19 @@ public class TC_001_GetGoogleMap_API_Test extends BaseClass{
 	
 
 	@Test
-	public void getMapAPI() {
-		System.out.println(properties.getProperty("google"));
+	public void getPlaceAPI() {
+		RestAssured.baseURI = properties.getProperty("SearchPlaceBaseUrl");
+		given()
+			.queryParam("input", "Museum%20of%20Contemporary%20Art%20Australia")
+			.queryParam("inputtype", "textquery")
+			.queryParam("fields", "photos,formatted_address,name,rating,opening_hours,geometry")
+			.queryParam("key", "AIzaSyD5UyJZknReiN5uJVzoLQoLva1NJV0yt_E")
+			.contentType(ContentType.JSON)
+		.when()
+			.get(properties.getProperty("SearchPlaceResources"))
+		.then()
+			.statusCode(200)
+			.contentType("application/json");
 		
 		
 	}
